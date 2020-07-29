f799a7b0f0aa94ad2e06a79ebb9bd906d504502c
Aldi Mustafri <aldimustafri@live.com>
2020-07-29 15:19:36 +0700

    update gist

--- MaximumProfit.js
+++ MaximumProfit.js
@@ -3,7 +3,7 @@
  * @email aldimustafri@live.com
  * @create date 2020-07-29 13:46:57
  * @modify date 2020-07-29 13:46:57
- * @desc [description]
+ * @desc https://gist.github.com/aldimustafri/d21b3d6058d5dfbe5920331c9d50bcdf
  */
 
 const url = `https://gist.githubusercontent.com/Jekiwijaya/c72c2de532203965bf818e5a4e5e43e3/raw/2631344d08b044a4b833caeab8a42486b87cc19a/gistfile1.txt`;
